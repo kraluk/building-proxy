d6e1b2b10f51217382e0dc1fd14b7acd3a90f033
Łukasz Krauzowicz <kraluk@gmail.com>
2024-11-11 11:16:20 +0100

    feat: add missing openapi/springdoc description to rest controllers

--- src/main/kotlin/io/kraluk/buildingproxy/adapter/building/controller/BuildingsController.kt
+++ src/main/kotlin/io/kraluk/buildingproxy/adapter/building/controller/BuildingsController.kt
@@ -1,22 +1,57 @@
 package io.kraluk.buildingproxy.adapter.building.controller
 
+import io.kraluk.buildingproxy.shared.contract.http.BuildingHttp
 import io.kraluk.buildingproxy.shared.contract.http.toHttp
 import io.kraluk.buildingproxy.usecase.building.FindBuildingByIdUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Buldings Proxy API")
 @RestController
 class BuildingsController(
   private val findByIsUseCase: FindBuildingByIdUseCase,
 ) {
 
-  @GetMapping(value = ["/v1/buildings/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-  fun findById(@PathVariable("id") id: Long): ResponseEntity<out Any> =
+  @Operation(
+    summary = "Find Building by Id via proxing to the Building API",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Found a building",
+        content = [
+          Content(
+            mediaType = APPLICATION_JSON_VALUE,
+            schema = Schema(implementation = BuildingHttp::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Building not found",
+        content = [
+          Content(
+            mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+            schema = Schema(implementation = ProblemDetail::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  @GetMapping(value = ["/v1/buildings/{id}"], produces = [APPLICATION_JSON_VALUE])
+  fun findById(@Parameter(description = "Id of the Building", example = "34567") @PathVariable("id") id: Long): ResponseEntity<out Any> =
     findByIsUseCase.invoke(FindBuildingByIdUseCase.Command(id))
       ?.toHttp()
       ?.let { ResponseEntity.ok(it) }
