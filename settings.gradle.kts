1524dfe2725856cf27df47661510d517b2e59a39
Łukasz Krauzowicz <kraluk@gmail.com>
2025-04-03 23:06:01 +0200

    chore: bump up spring boot to 3.4.4 and gradle to 8.13

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,24 +11,24 @@ dependencyResolutionManagement {
           plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").version("2.0.21")
           plugin("kotlin.spring", "org.jetbrains.kotlin.plugin.spring").version("2.0.21")
 
-          plugin("spring.boot", "org.springframework.boot").version("3.4.2")
+          plugin("spring.boot", "org.springframework.boot").version("3.4.4")
           plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
 
-          plugin("ktlint", "org.jlleitschuh.gradle.ktlint").version("12.1.2")
-          plugin("detekt", "io.gitlab.arturbosch.detekt").version("1.23.7")
+          plugin("ktlint", "org.jlleitschuh.gradle.ktlint").version("12.2.0")
+          plugin("detekt", "io.gitlab.arturbosch.detekt").version("1.23.8")
 
           plugin("versions", "com.github.ben-manes.versions").version("0.52.0")
 
-          version("springDoc", "2.8.4")
+          version("springDoc", "2.8.6")
           version("resilience4j", "2.3.0")
-          version("redisson", "3.44.0")
+          version("redisson", "3.45.1")
         }
         create("testLibs") {
-          version("awaitility", "4.2.2")
+          version("awaitility", "4.3.0")
           version("kluent", "1.73")
           version("mockitoKotlin", "5.4.0")
           version("jsonassert", "2.0-rc1")
-          version("springCloudContractWiremock", "4.2.0")
+          version("springCloudContractWiremock", "4.2.1")
         }
         create("toolLibs") {
           version("ktlint", "0.50.0")
