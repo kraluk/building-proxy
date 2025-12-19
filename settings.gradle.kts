bb95aa4950f8df103b8abd92687a66fc7e9354b0
Łukasz Krauzowicz <kraluk@gmail.com>
2025-12-19 23:07:57 +0100

    chore(deps): bump up spring boot to 3.5.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
       plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").version("2.2.21")
       plugin("kotlin.spring", "org.jetbrains.kotlin.plugin.spring").version("2.2.21")
 
-      plugin("spring.boot", "org.springframework.boot").version("3.5.8")
+      plugin("spring.boot", "org.springframework.boot").version("3.5.9")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
 
       plugin("ktlint", "org.jlleitschuh.gradle.ktlint").version("14.0.1")
@@ -26,8 +26,8 @@ dependencyResolutionManagement {
       version("kluent", "1.73")
       version("mockitoKotlin", "6.1.0")
       version("jsonassert", "2.0-rc1")
-      version("springCloudContractWiremock", "4.3.0")
-      version("testcontainers", "2.0.2")
+      version("springCloudContractWiremock", "4.3.1")
+      version("testcontainers", "2.0.3")
     }
     create("toolLibs") {
       version("ktlint", "1.8.0")
