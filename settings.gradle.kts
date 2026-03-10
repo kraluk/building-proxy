f508d557273466e1fdcf5e4a66ca4e3144e865b4
Łukasz Krauzowicz <kraluk@gmail.com>
2026-03-10 23:11:29 +0100

    chore(deps): bump up spring boot to 4.0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,17 +9,17 @@ dependencyResolutionManagement {
       plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").version("2.3.0")
       plugin("kotlin.spring", "org.jetbrains.kotlin.plugin.spring").version("2.3.0")
 
-      plugin("spring.boot", "org.springframework.boot").version("4.0.2")
+      plugin("spring.boot", "org.springframework.boot").version("4.0.3")
       plugin("spring.dependencyManagement", "io.spring.dependency-management").version("1.1.7")
 
-      plugin("ktlint", "org.jlleitschuh.gradle.ktlint").version("14.0.1")
+      plugin("ktlint", "org.jlleitschuh.gradle.ktlint").version("14.1.0")
       plugin("detekt", "io.gitlab.arturbosch.detekt").version("1.23.8")
 
       plugin("versions", "com.github.ben-manes.versions").version("0.53.0")
 
-      version("springDoc", "3.0.1")
+      version("springDoc", "3.0.2")
       version("resilience4j", "2.3.0")
-      version("redisson", "4.1.0")
+      version("redisson", "4.3.0")
     }
     create("testLibs") {
       version("awaitility", "4.3.0")
